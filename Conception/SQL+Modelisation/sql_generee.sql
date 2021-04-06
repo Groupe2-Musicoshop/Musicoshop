7847eba46134decf56eab980d58cf84c440391af
Pierre LANGE <pierre.lange@gmail.com>
2021-04-06 18:28:53 +0200

    modif du sql pour générer des tables sql avec des minuscules

--- Conception/SQL+Modelisation/sql_generee.sql
+++ Conception/SQL+Modelisation/sql_generee.sql
@@ -4,13 +4,13 @@ CREATE DATABASE musicoshop;
 
 USE musicoshop;
 
-CREATE TABLE Categorie(
+CREATE TABLE categorie(
    idCategorie INT,
    libele VARCHAR(50),
    PRIMARY KEY(idCategorie)
 );
 
-CREATE TABLE Commande(
+CREATE TABLE commande(
    idCmd INT,
    dateCmd VARCHAR(50),
    total DECIMAL(15,2),
@@ -18,14 +18,14 @@ CREATE TABLE Commande(
    PRIMARY KEY(idCmd)
 );
 
-CREATE TABLE Panier(
+CREATE TABLE panier(
    Id_Panier INT,
    idUtilisateur INT,
    idArticle INT,
    PRIMARY KEY(Id_Panier, idUtilisateur, idArticle)
 );
 
-CREATE TABLE Instruments(
+CREATE TABLE instruments(
    Id_Instruments INT,
    designation VARCHAR(50),
    prix VARCHAR(50),
@@ -33,10 +33,10 @@ CREATE TABLE Instruments(
    description VARCHAR(50),
    idCategorie INT NOT NULL,
    PRIMARY KEY(Id_Instruments),
-   FOREIGN KEY(idCategorie) REFERENCES Categorie(idCategorie)
+   FOREIGN KEY(idCategorie) REFERENCES categorie(idCategorie)
 );
 
-CREATE TABLE Article(
+CREATE TABLE article(
    Id_Article INT,
    designation VARCHAR(50),
    qtestock INT,
@@ -46,10 +46,10 @@ CREATE TABLE Article(
    description VARCHAR(50),
    Id_Instruments INT NOT NULL,
    PRIMARY KEY(Id_Article),
-   FOREIGN KEY(Id_Instruments) REFERENCES Instruments(Id_Instruments)
+   FOREIGN KEY(Id_Instruments) REFERENCES instruments(Id_Instruments)
 );
 
-CREATE TABLE Utilisateur(
+CREATE TABLE utilisateur(
    idUtilisateur INT,
    mail VARCHAR(50),
    login VARCHAR(50),
@@ -63,13 +63,13 @@ CREATE TABLE Utilisateur(
    codePostal INT,
    idCmd INT NOT NULL,
    PRIMARY KEY(idUtilisateur),
-   FOREIGN KEY(idCmd) REFERENCES Commande(idCmd)
+   FOREIGN KEY(idCmd) REFERENCES commande(idCmd)
 );
 
-CREATE TABLE Ligne_Commande(
+CREATE TABLE ligne_Commande(
    Id_Article INT,
    idCmd INT,
    PRIMARY KEY(Id_Article, idCmd),
-   FOREIGN KEY(Id_Article) REFERENCES Article(Id_Article),
-   FOREIGN KEY(idCmd) REFERENCES Commande(idCmd)
+   FOREIGN KEY(Id_Article) REFERENCES article(Id_Article),
+   FOREIGN KEY(idCmd) REFERENCES commande(idCmd)
 );
