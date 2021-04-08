077b2e29fd7f811ec1fd50dbc0c28be66c0d0b76
Pierre LANGE <pierre.lange@gmail.com>
2021-04-08 11:52:24 +0200

    maj footer

--- vues/footer.php
+++ vues/footer.php
@@ -5,17 +5,50 @@
 <div id="footer" class="bg-color-bzb">
     <div class="row">
 
-        <div class="col-sd-12 col-md-6 col-xl-4">
+        <div class="col-footer col-sd-12 col-md-6 col-xl-4">
             <ul id="cat-footer" class="mr-auto">
+                <h3>Catégories</h3> 
                 <?php $cat->genCategoriesVerticaly()?>
             </ul>
         </div>
 
-        <div class="col-sd-12 col-md-6 col-xl-4">
+        <div class="col-footer col-sd-12 col-md-6 col-xl-4">
             <?php require_once 'vues/social_footer.php';?>
         </div>
 
-        <div class="col-sd-12 col-md-6 col-xl-4"></div>
+        <div class="col-footer col-sd-12 col-md-6 col-xl-4">
+            <div id="paiements">
+
+                <h3>Paiements et sécurité</h3> 
+
+                <div>
+                    <span></span>
+                    <img src="https://www.datatrans.ch/media/filer_public/74/d7/74d7c2d0-6298-4b9d-a996-5136f61a3b21/paypal.svg" srcset="" alt="Datatrans AG – Wallets | PayPal (International)">
+
+                    <span></span>
+                    <img src="https://www.datatrans.ch/media/filer_public/cf/3c/cf3c542d-7c27-43b2-b075-d70d89026932/amazon-pay.svg" srcset="" alt="Datatrans AG – Wallets | Amazon Pay">
+                </div>   
+                
+                <div>
+                    <span></span> 
+                    <img src="https://www.datatrans.ch/media/filer_public/46/f7/46f7604c-d3e2-470a-a946-f15e5ba1226f/visa.svg" srcset="" alt="Datatrans AG – Cartes de Crédit | Visa (International)">
+
+                    <span></span>
+                    <img src="https://www.datatrans.ch/media/filer_public/26/2a/262ab488-b418-461b-9186-bf3263104440/mastercard.svg" srcset="" alt="Datatrans AG – Cartes de Crédit | Mastercard (International)">
+                </div> 
+
+                <div>
+                    <span></span>
+                    <img src="https://www.datatrans.ch/media/filer_public/c0/8a/c08a5d06-23a2-4ce5-9e83-a5da086b00a9/card_amex-old.svg" srcset="" alt="Datatrans AG – Cartes de Crédit | American Express (International)">
+
+                    <span></span>
+                    <img src="https://www.datatrans.ch/media/filer_public/06/89/06898aeb-f744-4082-a8dd-8d0353a43df0/diners.svg" srcset="" alt="Datatrans AG – Cartes de Crédit | Diners Club (International)">
+                </div>  
+    
+                </a> <p>Réglez de manière sûre et sécurisée par Carte bancaire, Virement, PayPal ou Amazon Pay.</p>  
+
+            </div>
+        </div>
     </div>
 </div>
 </div>
