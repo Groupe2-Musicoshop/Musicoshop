8157c3d12de067100a1dd74de397161068b5e1b2
Pierre LANGE <pierre.lange@gmail.com>
2021-04-09 11:58:22 +0200

    ajout stars

--- modele/Article.php
+++ modele/Article.php
@@ -141,25 +141,29 @@ class Article{
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
             extract($row);
             echo '<div class="card cat'.$row["idCategorie"].' col-md-4" data-category="cat'.$row['idCategorie'].'">';
-                echo '<div class="box_img">';
-                echo '<span class="helper"></span>';
-                    echo '<img src="'.$row['img'].'" class="img_thumb card-img-top" alt="">';
-                echo '</div>';
-                echo '<div class="card-body row">';
-                    echo '<div class="col-md-8">';
-                        echo '<h5 class="card-title">'.ucfirst($row['designation']).'</h5>';
-                        /*echo "<img src='".$_SESSION['root']."/img/article/star.svg' class='img_thumb star card-img-top'
-alt=''>";*/
-                echo '<a href="" class="btn btn-primary ">Lire plus</a>';
-                echo '</div>';
-                echo '<div class="col-md-4">';
-                    echo '<h5>'.$row['prix'].' €</h5>';
-                    echo '<a class="btn btn-success" href=""><i class="fa fa-cart-plus"></i></a>';
-                    echo '</div>';
-                echo '</div>';
-                echo '</div>';
+            echo '<div class="box_img">';
+            echo '<span class="helper"></span>';
+                echo '<img src="'.$row['img'].'" class="img_thumb card-img-top" alt="">';
+            echo '</div>';
+            echo '<div class="card-body row">';
+            echo '<div class="col-md-8">';
+            echo '<h5 class="card-title">'.ucfirst($row['designation']).'</h5>';
+            echo '<h6>';
+            for ($i = 1; $i <= $row['note']; $i++) {
+                echo '<img src="'.$_SESSION['root'].'/img/article/star.svg" class="img_thumb star card-img-top" alt="">';
+            }
+            echo '</h6>';
 
-                }
+echo '<a href="" class="btn btn-primary ">Lire plus</a>';
+echo '</div>';
+echo '<div class="col-md-4">';
+    echo '<h5>'.$row['prix'].' €</h5>';
+    echo '<a class="btn btn-success" href=""><i class="fa fa-cart-plus"></i></a>';
+    echo '</div>';
+echo '</div>';
+echo '</div>';
+
+}
 }
 
 public function getSqlArticles(){
