b89915725f0c53ef6fca53521057872269284402
Pierre LANGE <pierre.lange@gmail.com>
2021-04-20 05:08:59 +0200

    installation de la base sur heroku

--- modele/Database.php
+++ modele/Database.php
@@ -1,9 +1,14 @@
 <?php
     class Database {
-        private $host = "127.0.0.1";
+        /*private $host = "127.0.0.1";
         private $database_name = "musicoshop";
         private $username = "root";
-        private $password = "";
+        private $password = "";*/
+
+        private $host = "f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+        private $database_name = "eb5ttazpjy6sdthk";
+        private $username = "ga45v6n3f1i182fu";
+        private $password = "hvrox0jwn17yschv";
 
         public $conn;
 
